e10f3a6904b6990880020a78146201a01829013c
Lucas Teixeira Vieira <lucas.vieira@brmalls.com.br>
2022-01-26 18:34:39 -0300

    Configurando MySQL e persistindo dados via POST

--- controllers/antendimentos.js
+++ controllers/antendimentos.js
@@ -1,8 +1,11 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos.'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento)
         res.send('Você está realizando um post.')
     })
 }
