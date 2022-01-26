49dce3f19c1d23c3211de3deec967b569adbadf8
Lucas Teixeira Vieira <lucas.vieira@brmalls.com.br>
2022-01-26 16:59:08 -0300

    Configurando recebimentos de dados via POST

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const consig = require('consign')
+const bodyParser = require('body-parser')
 
 module.exports = () => {
     const app = express()
+
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
 
     consig()
         .include('controllers')
