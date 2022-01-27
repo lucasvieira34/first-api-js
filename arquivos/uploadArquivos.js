c8ebd5f7828be7b27863a49d0b06b5397361ff53
Lucas Teixeira Vieira <lucas.vieira@brmalls.com.br>
2022-01-27 15:17:08 -0300

    Corrigindo e validando tipo de arquivo

--- arquivos/uploadArquivos.js
+++ arquivos/uploadArquivos.js
@@ -1,8 +1,21 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = (caminho, nomeArquivo, callbackImagemCriada) => {
-    const novoCaminho = `./assets/imagens/${nomeArquivo}`
-    fs.createReadStream(caminho)
-        .pipe(fs.createWriteStream(novoCaminho))
-        .on('finish', () => callbackImagemCriada(novoCaminho))
+
+    const tipoValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoValido = tipoValidos.indexOf(tipo.substring(1)) !== -1
+
+    if (tipoValido) {
+        const novoCaminho = `./assets/imagens/${nomeArquivo}${tipo}`
+
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(false, novoCaminho))
+    } else {
+        const erro = "Tipo é inválido"
+        console.error('Erro. Tipo inválido.')
+        callbackImagemCriada(erro)
+    }
 }
--- models/pets.js
+++ models/pets.js
@@ -5,16 +5,21 @@ class Pet {
     adicionar(pet, res) {
         const query = 'INSERT INTO Pets SET ?'
 
-        uploadArquivo(pet.imagem, pet.nome, (novoCaminho) => {
-            const novoPet = {nome: pet.nome, imagem: novoCaminho}
-            conexao.query(query, novoPet, (erro) => {
-                if(erro){
-                    console.log(erro)
-                    res.status(400).json(erro)
-                } else {
-                    res.status(200).json(novoPet)
-                }
-            })
+        uploadArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+
+            if(erro) {
+                res.status(400).json({ erro })
+            } else {
+                const novoPet = {nome: pet.nome, imagem: novoCaminho}
+                conexao.query(query, novoPet, (erro) => {
+                    if(erro){
+                        console.log(erro)
+                        res.status(400).json(erro)
+                    } else {
+                        res.status(200).json(novoPet)
+                    }
+                })
+            }
         })
     }
 }
