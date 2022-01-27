9f07a555d541788c5599c3764597e29f9875edf8
Lucas Teixeira Vieira <lucas.vieira@brmalls.com.br>
2022-01-26 22:48:07 -0300

    Ajustando status HTTP de retorno

--- controllers/antendimentos.js
+++ controllers/antendimentos.js
@@ -5,7 +5,6 @@ module.exports = app => {
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
-        Atendimento.adiciona(atendimento)
-        res.send('Você está realizando um post.')
+        Atendimento.adiciona(atendimento, res)
     })
 }
